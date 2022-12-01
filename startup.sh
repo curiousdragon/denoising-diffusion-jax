a31418aafef472efb74dc4bb9376b2773e0cd757
Jamie Hong <jamiehong@berkeley.edu>
2022-12-01 12:10:32 -0800

    [code] Update startup script slightly

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 echo "create conda environment with jax"
-yes | conda create -n env-proj
+yes | conda create -n env-proj jupyter pip
 conda activate env-proj
 pip3 install --upgrade pip
 pip3 install --upgrade "jax[cpu]"
+pip3 install --upgrade ipympl
+pip3 install --upgrade matplotlib
