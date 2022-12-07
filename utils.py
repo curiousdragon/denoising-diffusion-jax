32be03907aeaf6211f6279cd7e6576afcbaa8044
Jamie Hong <jamiehong@berkeley.edu>
2022-12-06 22:29:18 -0800

    [code] Fix generator, add more visualizations

--- utils.py
+++ utils.py
@@ -4,17 +4,11 @@ from jax.config import config
 config.update("jax_array", True)
 
 import matplotlib.pyplot as plt
-from typing import List
 
 
 def show_outputs(states: Array):
     n = len(states)
     _, axs = plt.subplots(nrows=1, ncols=n, figsize=(9, 2))
     for i in range(n):
-        axs[i].imshow(states[i])
+        axs[i].imshow(states[i], vmin=-1, vmax=2)
     plt.show()
-
-
-def show_states(state_visuals: List[List[Array]]):
-    for states in state_visuals:
-        show_outputs(states)
